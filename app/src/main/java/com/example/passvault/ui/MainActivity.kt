005cf6cffd6376af1c2c9395bddfb90c980b1e71
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-06-08 23:50:40 +0530

    Initial Commit

--- app/src/main/java/com/example/passvault/ui/MainActivity.kt
+++ app/src/main/java/com/example/passvault/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.passvault
+package com.example.passvault.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.passvault.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
