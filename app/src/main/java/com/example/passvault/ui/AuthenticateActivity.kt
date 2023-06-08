5e0a00b30bc87afc5b3638dd9dea0b8d23792a07
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-06-09 01:54:15 +0530

    Create AuthenticateActivity

---
+++ app/src/main/java/com/example/passvault/ui/AuthenticateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.passvault.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.passvault.R
+
+class AuthenticateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authenticate)
+    }
+}
