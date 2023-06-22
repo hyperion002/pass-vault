9ac860864d9f886dcda89b6abc0beb8fd22efe4e
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-06-23 01:21:10 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/passvault/ui/fragments/VaultFragment.kt
@@ -0,0 +1,19 @@
+package com.example.passvault.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.passvault.R
+
+class VaultFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vault, container, false)
+    }
+}
